57e00eddb61ba13deeac38ac3e654547deaae8b0
alexandra1207 <pekarinaa@gmail.com>
2021-12-28 17:08:14 +0300

    edits added

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,10 @@ $(function () {
   $('.top-slider__list').slick ({
     prevArrow: '<button type="button" class="slick-prev"><svg class="arrow-left"><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="arrow-right"><use xlink:href="images/sprite.svg#arrow-right"></use></svg></button>',
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // infinite: false
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: false
   });
 
-  var mixer = mixitup('.top-products__content');
+  var mixer = mixitup('.top__content');
 })
