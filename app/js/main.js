aad6b1ee252751ec593f6100b484e8cd985f295c
alexandra1207 <pekarinaa@gmail.com>
2022-01-29 09:07:08 +0300

    additional task

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 
-
 $(function () {
+
+  $('.product__num').styler();
 
   $('.top-slider__list').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg class="arrow-left"><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
@@ -22,16 +23,53 @@ $(function () {
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
 
+  // Не получается сделать так, чтобы меню каталога закрывалось при клике вне него. Заменить toggleClass на addClass и removeClass не получилось, не работает. Закоментировала тот код, который по идее должен работать, а он не работает ((
 
-  $('.partners__list').slick ({
-    arrows: false,
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    autoplay: true,
-    autoplaySpeed: 2000
-  })
+  $('.header__catalog-btn, .catalog__link').on('click', function () {
+    $('.catalog, .header__catalog-btn').toggleClass('catalog--active');
+  });
 
+  //   $('.header__catalog-btn').on ('click', function() {
+  //   $('.catalog').addClass('catalog--active');
+  // });
+
+  // $('.header__catalog-btn').on('click', function () {
+  //   $('.catalog, .catalog__link').removeClass('catalog--active');
+  // });
+
+
+  // let btnCatalog = $('.header__catalog-btn');
+  // let catalog = $('.catalog');
+
+  // $(document).on(click, function (e) {
+  //   if (!btnCatalog.is(e.target) && btnCatalog.has(e.target).length === 0 &&
+  //       !catalog.is(e.target) && catalog.has(e.target).length === 0) {
+  //     catalog.removeClass('catalog--active'); 
+  //   }
+  // });
+
+
+  $('.user-nav__link').on('click', function () {
+    $('.cart').addClass('cart--active');
+  });
+
+  $('.cart__btn-close').on('click', function () {
+    $('.cart').removeClass('cart--active');
+  });
+
+
+  // липкий хэдер .Не смогла придумать нормального решения для замещения места под липкий хэдер.Сделала через border - bottom у header__top.Тупое решение, но я не знаю, что еще придумать
+
+
+  let header = $('.header__content');
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      header.addClass('header__content--fixed');
+    } else {
+      header.removeClass('header__content--fixed');
+    }
+  });
 
 });
 
@@ -42,7 +80,7 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 140,
   autoplay: true,
   autoplaySpeed: 3000,
-  slidesPerGroup: 6 
+  slidesPerGroup: 6
 });
 
 
