07408bb37973d1eb6fdc839e14f7a7ef3a151f50
alexandra1207 <pekarinaa@gmail.com>
2022-03-23 15:03:18 +0300

    scroll

--- app/js/main.js
+++ app/js/main.js
@@ -105,17 +105,80 @@ $(function () {
   });
 
 
-  $('.filter-price__input').ionRangeSlider({
-    type: 'double',
-    onStart: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
-    onChange: function (data) {
-      $('.filter-price__from').text(data.from);
-      $('.filter-price__to').text(data.to);
-    },
+  // ionRangeSlider
+  
+
+  let $range = $(".js-range-slider"),
+    $inputFrom = $(".js-input-from"),
+    $inputTo = $(".js-input-to"),
+    instance,
+    min = 0,
+    max = 10000000,
+    from = 0,
+    to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    onStart: updateInputs,
+    onChange: updateInputs
   });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+  });
+
+  $inputTo.on("input", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+  });
+
+
+  // $('.filter-price__input').ionRangeSlider({
+  //   type: 'double',
+  //   onStart: function (data) {
+  //     $('.filter-price__from').text(data.from);
+  //     $('.filter-price__to').text(data.to);
+  //   },
+  //   onChange: function (data) {
+  //     $('.filter-price__from').text(data.from);
+  //     $('.filter-price__to').text(data.to);
+  //   },
+  // });
+
+
 
   $('.product__num').styler();
 
